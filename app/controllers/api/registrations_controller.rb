0160486afd35f2d7001b12e6a1e75d2ed6bed0cc
Luciano Uribe <lucianouribe@gmail.com>
2017-04-28 16:57:26 -0500

    back end authentication

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,2 +1,20 @@
-class Api::RegistrationsController < ApplicationController
+class Api::RegistrationsController < Devise::RegistrationsController
+  skip_before_action :verify_authenticity_token
+  clear_respond_to
+  respond_to :json
+
+  private
+
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :role, :email, :verify_email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:first_name, :last_name, :role, :email, :verify_email, :password, :password_confirmation, :current_password)
+    end
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
 end
