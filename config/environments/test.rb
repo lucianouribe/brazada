e41503fb4703654678cfa08186061595b540040d
Luciano Uribe <lucianouribe@gmail.com>
2017-07-25 13:35:54 -0500

    devdep in package json

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=3600'
   }
-  config.webpack.dev_server.enabled = !ENV['CI']
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
